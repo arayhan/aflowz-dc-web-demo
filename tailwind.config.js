0c7a0aa8f1eaef81cecf51aa68918765eee42d18
Rayhan <rayhanprima99@gmail.com>
2022-07-28 17:01:28 +0700

    chore: remove redundant files & setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      colors: {
+        primary: '#0070f3',
+        secondary: '#ff4081',
+      },
+      fontFamily: {
+        heading: ['Plus Jakarta Sans', 'sans-serif'],
+        body: ['Plus Jakarta Sans', 'sans-serif'],
+        monospace: ['Fira Code', 'monospace'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
