47a33e08c557e311f8118a843b75a25c6149ed89
Rayhan <rayhanprima99@gmail.com>
2022-07-28 17:09:09 +0700

    chore: test tailwind

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="font-semibold text-5xl text-center bg-white p-20 rounded-lg shadow-md">HOME</h1>
     </div>
   );
 }
